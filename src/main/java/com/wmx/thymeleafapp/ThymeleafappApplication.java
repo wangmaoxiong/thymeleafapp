e50e35f88ae9d90aef2834ff8dcfe20d3e4ebacd
wangmaoxiong <2268461750@qq.com>
2020-03-20 22:21:28 +0800

    初始化项目

---
+++ src/main/java/com/wmx/thymeleafapp/ThymeleafappApplication.java
@@ -0,0 +1,13 @@
+package com.wmx.thymeleafapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafappApplication.class, args);
+    }
+
+}
