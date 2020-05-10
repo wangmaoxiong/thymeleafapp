07913685bb2518260a1ae9e0a870ee884f8123dc
wangmaoxiong <2268461750@qq.com>
2020-05-10 10:19:36 +0800

    @MapperScan 扫描 MyBatis 的 Mapper 接口.

--- src/main/java/com/wmx/thymeleafapp/ThymeleafappApplication.java
+++ src/main/java/com/wmx/thymeleafapp/ThymeleafappApplication.java
@@ -4,6 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wangmaoxiong
+ * @version 1.0
+ * @date 2020/5/10 9:50
+ * {@link MapperScan} 指定 MyBatis 扫描的 Mapper 接口目录.
+ */
 @SpringBootApplication
 @MapperScan(value = {"com.wmx.thymeleafapp.mapper"})
 public class ThymeleafappApplication {
