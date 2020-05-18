4f5dbaf5dfc424cc2a4e3da274735865e2fb34af
wangmaoxiong <2268461750@qq.com>
2020-05-18 21:09:01 +0800

    对文件提取摘要

--- src/main/java/com/wmx/thymeleafapp/utils/Md5EncodeUtils.java
+++ src/main/java/com/wmx/thymeleafapp/utils/Md5EncodeUtils.java
@@ -2,9 +2,12 @@ package com.wmx.thymeleafapp.utils;
 
 import sun.misc.BASE64Encoder;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 
 /**
  * 新增摘要工具类
@@ -27,8 +30,8 @@ public class Md5EncodeUtils {
         try {
             algorithm = algorithm == null ? "MD5" : algorithm;
             //指定信息摘要算法提取摘要的哈希值. 哈希值字节数组，如果直接 new String(md5Byte) 是会乱码的
-            MessageDigest md5 = MessageDigest.getInstance(algorithm);
-            byte[] md5Byte = md5.digest(message.getBytes());
+            MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+            byte[] md5Byte = messageDigest.digest(message.getBytes());
             //使用 BASE64 进行定长编码
             BASE64Encoder base64Encoder = new BASE64Encoder();
             result = base64Encoder.encode(md5Byte);
@@ -39,8 +42,84 @@ public class Md5EncodeUtils {
     }
 
 
+    /**
+     * 对文件提取摘要
+     *
+     * @param file      ：待提取摘要的文件
+     * @param algorithm 生成摘要的算法，默认为  md5，可选值有  SHA-1、SHA-256、MD5
+     * @return
+     */
+    public static String md5DigestByFile(File file, String algorithm) {
+        String result = "";
+        try {
+            algorithm = algorithm == null ? "MD5" : algorithm;
+            //构建文件输入流，然后获取文件通道。
+            FileInputStream fileInputStream = new FileInputStream(file);
+            //FileChannel 用于读取，写入，映射和操作文件的通道。
+            FileChannel fileChannel = fileInputStream.getChannel();
+            /**
+             * map(MapMode mode,long position, long size)：将此频道文件的区域直接映射到内存中。
+             * position - 映射区域要启动的文件中的位置; 必须是非负的
+             * size - 要映射的区域的大小; 必须是非负数，不得大于Integer.MAX_VALUE
+             */
+            MappedByteBuffer byteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, file.length());
+            //指定信息摘要算法提取摘要的哈希值. 哈希值字节数组，如果直接 new String(md5Byte) 是会乱码的
+            MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+            //使用指定的 ByteBuffer 更新摘要.
+            messageDigest.update(byteBuffer);
+            //提取摘要，然后将十进制的字节数组转成 16 进制可视字符串.
+            //通常在网上下载的资源，对方提供对照的 md5 码，或者 sha 码，也是使用 16 进制转换的.
+            result = bytes2HexString(messageDigest.digest(), false);
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 10 进制字节数组转 16 进制字符串（因为16进制中含有A-F，所以只能用字符串表示）
+     * 对应上面的 hexString2Bytes 方法
+     *
+     * @param b           :待转换的10进制字节数组，如 new byte[]{69, 83, 67, 47, 86, 80, 46, 110, 101, 116, 16, 3, 0, 0, 0, 0};
+     * @param isHaveBlank 转换的结果是否用空格隔开，true 时如 "45 5A 43 2F 56 00"，false 时如 "455A432F5600"
+     * @return 转换好的 16进制字符串，如 "45 53 43 2F 56 50 2E 6E 65 74 10 03 00 00 00 00 "
+     */
+    private static String bytes2HexString(byte[] b, boolean isHaveBlank) {
+        if (b == null || b.length <= 0) {
+            System.out.println("bytes2HexString 参数错误，放弃转换.");
+            return null;
+        }
+        StringBuffer result = new StringBuffer();
+        String hex;
+        for (int i = 0; i < b.length; i++) {
+            hex = Integer.toHexString(b[i] & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+            if (isHaveBlank) {
+                result.append(hex.toUpperCase() + " ");
+            } else {
+                result.append(hex.toUpperCase());
+            }
+        }
+        return result.toString();
+    }
 
     public static void main(String[] args) {
+
+        new Md5EncodeUtils().test1();
+//        new Md5EncodeUtils().test2();
+
+    }
+
+    public void test2() {
+        File file = new File("C:\\Users\\Think\\Downloads\\commons-io-2.6-bin.tar.gz");
+        String fileMd5Digest = md5DigestByFile(file, "SHA-256");
+        System.out.println("fileMd5Digest: " + fileMd5Digest.toLowerCase());
+    }
+
+    public void test1() {
         String sourceMessage = "123456万里长城_&*$#.Nice";
         //原字符：123456万里长城_&*$#.Nice
         System.out.println("原字符：" + sourceMessage);
